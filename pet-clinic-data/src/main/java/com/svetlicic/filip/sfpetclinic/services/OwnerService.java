5012903385a931dadf73e0cd1634490ca7439b9b
Geli4ever <38469600+Geli4ever@users.noreply.github.com>
2020-10-28 01:31:09 +0100

    Refactoring to common base interface. close #11

--- pet-clinic-data/src/main/java/com/svetlicic/filip/sfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/svetlicic/filip/sfpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.svetlicic.filip.sfpetclinic.services;
 
 import com.svetlicic.filip.sfpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/svetlicic/filip/sfpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/svetlicic/filip/sfpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package com.svetlicic.filip.sfpetclinic.services;
 
 import com.svetlicic.filip.sfpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
