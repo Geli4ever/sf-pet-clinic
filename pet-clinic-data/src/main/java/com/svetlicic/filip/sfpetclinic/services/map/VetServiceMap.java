1e1d43ee9660db81761a787e46e6026ee6fd64ef
Geli4ever <38469600+Geli4ever@users.noreply.github.com>
2020-10-28 02:09:09 +0100

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/com/svetlicic/filip/sfpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.svetlicic.filip.sfpetclinic.services.map;
+
+import com.svetlicic.filip.sfpetclinic.model.Vet;
+import com.svetlicic.filip.sfpetclinic.services.CrudService;
+
+import java.util.Set;
+
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
